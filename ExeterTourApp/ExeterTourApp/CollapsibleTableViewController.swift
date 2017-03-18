1bfaf2a2ec42daa3f25b6f6f1420111de2b47bb2
chanseo07 <baebrian07@gmail.com>
2017-03-18 15:35:51 +0900

    initial

---
+++ ExeterTourApp/ExeterTourApp/CollapsibleTableViewController.swift
@@ -0,0 +1,122 @@
+//
+//  CollapsibleTableViewController.swift
+//  ios-swift-collapsible-table-section
+//
+//  Created by Yong Su on 5/30/16.
+//  Copyright © 2016 Yong Su. All rights reserved.
+//
+
+import UIKit
+
+//
+// MARK: - Section Data Structure
+//
+struct Section {
+    var name: String!
+    var items: [String]!
+    var collapsed: Bool!
+    
+    init(name: String, items: [String], collapsed: Bool = false) {
+        self.name = name
+        self.items = items
+        self.collapsed = collapsed
+    }
+}
+
+//
+// MARK: - View Controller
+//
+class CollapsibleTableViewController: UITableViewController {
+    
+    var sections = [Section]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = "PEA Buildings"
+        
+        // Initialize the sections array
+        // Here we have three sections: Mac, iPad, iPhone
+        sections = [
+            Section(name: "Academic Buildings", items: ["Academy Building", "Phelps Science Center"]),
+            Section(name: "Student Life", items: ["dummy"]),
+            Section(name: "Athletics", items: ["dummy"]),
+            Section(name: "Admissions and Administration", items: ["dummy"]),
+            Section(name: "Dormitories", items: ["dummy"]),
+            Section(name: "Parking", items: ["dummy"])
+        ]
+    }
+    
+}
+
+//
+// MARK: - View Controller DataSource and Delegate
+//
+extension CollapsibleTableViewController {
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return sections.count
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sections[section].items.count
+    }
+    
+    // Cell
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as UITableViewCell? ?? UITableViewCell(style: .default, reuseIdentifier: "cell")
+        
+        cell.textLabel?.text = sections[(indexPath as NSIndexPath).section].items[(indexPath as NSIndexPath).row]
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return sections[(indexPath as NSIndexPath).section].collapsed! ? 0 : 44.0
+    }
+    
+    // Header
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let header = tableView.dequeueReusableHeaderFooterView(withIdentifier: "header") as? CollapsibleTableViewHeader ?? CollapsibleTableViewHeader(reuseIdentifier: "header")
+        
+        header.titleLabel.text = sections[section].name
+        header.arrowLabel.text = ">"
+        header.setCollapsed(sections[section].collapsed)
+        
+        header.section = section
+        header.delegate = self
+        
+        return header
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 44.0
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 1.0
+    }
+
+}
+
+//
+// MARK: - Section Header Delegate
+//
+extension CollapsibleTableViewController: CollapsibleTableViewHeaderDelegate {
+    
+    func toggleSection(_ header: CollapsibleTableViewHeader, section: Int) {
+        let collapsed = !sections[section].collapsed
+        
+        // Toggle collapse
+        sections[section].collapsed = collapsed
+        header.setCollapsed(collapsed)
+        
+        // Adjust the height of the rows inside the section
+        tableView.beginUpdates()
+        for i in 0 ..< sections[section].items.count {
+            tableView.reloadRows(at: [IndexPath(row: i, section: section)], with: .automatic)
+        }
+        tableView.endUpdates()
+    }
+    
+}
---
+++ ExeterTourApp/ExeterTourApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ExeterTourApp
+//
+//  Created by Brian Bae on 2017. 3. 18..
+//  Copyright © 2017년 bbae07. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
